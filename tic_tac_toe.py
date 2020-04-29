cdc354cc94cb52f3e8bcac40ec2d9f79edfc1b87
Sergei Kryzhanovskii <s.kryzhanovskii@gmail.com>
2020-04-29 15:45:46 +0300

    Исправил ошибки с зачёркиванием, расстановкой. Изменён цвет оформления

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,8 +7,9 @@ winner = None
 draw = False
 width = 400
 height = 400
-white = (255, 255, 255)
+background_color = (239, 228, 176)
 line_color = (10, 60, 73)
+check_line_color = (0, 250, 0)
 
 # доска 9х9
 TTT = [[None]*3, [None]*3, [None]*3]
@@ -17,49 +18,49 @@ TTT = [[None]*3, [None]*3, [None]*3]
 pg.init()
 fps = 30
 clock = pg.time.Clock()
-screen = pg.display.set_mode((width, height+100), 0, 32)
+screen = pg.display.set_mode((width, height + 100), 0, 32)
 pg.display.set_caption("Tic Tac Toe")
 
 # загрузка изображений
-start = pg.image.load('Tic Tac Toe.png')
+start_img = pg.image.load('Tic Tac Toe.png')
 x_img = pg.image.load('X.png')
 o_img = pg.image.load('O.png')
 
 # меняем размер изображения
-x_img = pg.transform.scale(x_img, (80,80))
-o_img = pg.transform.scale(o_img, (80,80))
-start = pg.transform.scale(start, (width, height+100))
+x_img = pg.transform.scale(x_img, (80, 80))
+o_img = pg.transform.scale(o_img, (80, 80))
+start_img = pg.transform.scale(start_img, (width, height+100))
 
 def game_opening():
-    screen.blit(start, (0,0))
+    # заставка, пауза, заливка фона
+    screen.blit(start_img, (0, 0))
     pg.display.update()
-    time.sleep(1)
-    screen.fill(white)
-
-    # прорисовка вертикальных линий
-    pg.draw.line(screen, line_color, (width/3,0), (width/3, height), 7)
-    pg.draw.line(screen, line_color, (width/3*2,0), (width/3*2, height), 7)
-    # прорисовка горизонтальных линий
-    pg.draw.line(screen,line_color,(0,height/3),(width,height/3),7)
-    pg.draw.line(screen, line_color,(0,height/3*2), (width, height/3*2), 7)
+    time.sleep(1.5)
+    screen.fill(background_color)
+    # прорисовка вертикальных линий поля
+    pg.draw.line(screen, line_color, (width/3, 0), (width/3, height), 7)
+    pg.draw.line(screen, line_color, (width/3*2, 0), (width/3*2, height), 7)
+    # прорисовка горизонтальных линий поля
+    pg.draw.line(screen, line_color, (0, height/3), (width, height/3), 7)
+    pg.draw.line(screen, line_color, (0, height/3*2), (width, height/3*2), 7)
     draw_status()
 
 
 def draw_status():
     global draw
 
+    # задать текст сообщения
     if winner is None:
         message = XO.upper() + "'s turn"
     else:
         message = winner.upper() + " won!"
     if draw:
         message = 'Game Draw!'
-
+    # задать шрифт
     font = pg.font.Font(None, 30)
     text = font.render(message, 1, (255, 255, 255))
-
     # копируем сообщение на доску
-    screen.fill((0, 0, 0), (0, 400, 500, 100))
+    screen.fill((10, 60, 73), (0, 400, 400, 100))
     text_rect = text.get_rect(center=(width/2, 500-50))
     screen.blit(text, text_rect)
     pg.display.update()
@@ -72,8 +73,8 @@ def check_win():
         if (TTT[row][0] == TTT[row][1] == TTT[row][2]) and (TTT[row][0] is not None):
             # эта строка победила
             winner = TTT[row][0]
-            pg.draw.line(screen, (250,0,0), (0, (row + 1)*height/3 - height/6),\
-                         (width, (row + 1)*height/3 - height/6), 4)
+            pg.draw.line(screen, check_line_color, ((row + 1) * width / 3 - width / 6, 0),
+                         ((row + 1) * width / 3 - width / 6, height), 4)
             break
 
     # проверка победителя по столбцу
@@ -81,21 +82,21 @@ def check_win():
         if (TTT[0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None):
             # этот столбец победил
             winner = TTT[0][col]
-            # рисуем линию победителя
-            pg.draw.line(screen, (250, 0, 0), (0, (col + 1) * height / 3 - height / 6),\
-                         ((col + 1) * height / 3 - height / 6), 4)
+            # рисуем линию
+            pg.draw.line(screen, check_line_color, (0, (col + 1) * height / 3 - height / 6),
+                         (width, (col + 1) * height / 3 - height / 6), 4)
             break
 
     # проверка победителя по диагонали
     if (TTT[0][0] == TTT[1][1] == TTT[2][2]) and (TTT[0][0] is not None):
         # выйграла диагноаль слева направо
         winner = TTT[0][0]
-        pg.draw.line(screen, (250, 70, 70), (50, 50), (350, 350), 4)
+        pg.draw.line(screen, check_line_color, (50, 50), (350, 350), 4)
 
     if (TTT[0][2] == TTT[1][1] == TTT[2][0]) and (TTT[0][2] is not None):
         # выйграла диагноаль справа налево
         winner = TTT[0][2]
-        pg.draw.line(screen, (250, 70, 70), (350, 50), (350, 50), 4)
+        pg.draw.line(screen, check_line_color, (350, 50), (50, 350), 4)
 
     if all([all(row) for row in TTT]) and winner is None:
         draw = True
@@ -131,7 +132,7 @@ def drawXO(row, col):
 
 def userClick():
     # получаем координаты компьютерной мыши
-    x, y = pg.mouse.get_pos()
+    y, x = pg.mouse.get_pos()
 
     # выясняем номер колонки
     if x < width / 3:
